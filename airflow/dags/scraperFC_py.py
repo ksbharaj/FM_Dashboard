7b8205a929752e23e5a85ca5e74df12178d54b33
ksbharaj <kbharaj3@gatech.edu>
2023-12-19 23:11:29 -0500

    xPts table and Airflow DAG updates!

--- airflow/dags/scraperFC_py.py
+++ airflow/dags/scraperFC_py.py
@@ -3,26 +3,32 @@ import pandas as pd
 from tqdm import tqdm
 import ScraperFC as sfc
 import traceback
+from google.cloud import storage
+import os
 
-import warnings
-import uuid
+def upload_to_gcs(bucket_name, object_name, local_file):
+    """Uploads a file to Google Cloud Storage."""
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(object_name)
+    blob.upload_from_filename(local_file)
 
 def scraperFC_FBRef():
     scraper = sfc.FBRef()
     try:
-        # Scrape the table
         lg_table = scraper.scrape_league_table(year=2023, league='EPL')
-    except:
-        # Catch and print any exceptions. This allows us to still close the
-        # scraper below, even if an exception occurs.
+    except Exception as e:
         traceback.print_exc()
+        raise
     finally:
-        # It's important to close the scraper when you're done with it. Otherwise,
-        # you'll have a bunch of webdrivers open and running in the background.
         scraper.close()
-        
-    lg_table.sort_values(by='Squad', ascending=True, inplace=True)
 
+    lg_table.sort_values(by='Squad', ascending=True, inplace=True)
     lg_table.reset_index(drop=True, inplace=True)
 
-    return (lg_table)
+    file_path = '/tmp/league_table.csv'
+    lg_table.to_csv(file_path, index=False)
+
+    upload_to_gcs('gegenstats', 'league_table.csv', file_path)
+
+    return 'gs://gegenstats/league_table.csv'
