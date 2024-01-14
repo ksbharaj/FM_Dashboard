adf16b6c0424a652935082d30da3c9884a5c00dc
ksbharaj <kbharaj3@gatech.edu>
2024-01-13 21:34:29 -0500

    Inclusion of secrets to hide my Snowflake password

--- myproject/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-# myapp/urls.py
-from django.urls import path
-from .views import radar_chart_view, ajax_radar_chart_view
-
-urlpatterns = [
-    path('radar_chart/', radar_chart_view, name='radar_chart'),
-    path('ajax/radar_chart/', ajax_radar_chart_view, name='ajax_radar_chart'),
-]
